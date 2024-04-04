f7f9f44d3c005d4fb49ab75bdd15385110e35eb3
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-04 14:09:04 +0300

    update

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,9 @@
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log("Gallery");
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+console.log(SimpleLightbox);
